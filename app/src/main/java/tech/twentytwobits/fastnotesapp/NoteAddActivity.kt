7345456b42081f38213e77c0dfe6bf1091f27b7e
Héctor Sabillón <h.hsabillon@gmail.com>
2019-12-02 10:51:45 -0600

    Layout para el template del recycler view de notas

---
+++ app/src/main/java/tech/twentytwobits/fastnotesapp/NoteAddActivity.kt
@@ -0,0 +1,12 @@
+package tech.twentytwobits.fastnotesapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class NoteAddActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_note_add)
+    }
+}
